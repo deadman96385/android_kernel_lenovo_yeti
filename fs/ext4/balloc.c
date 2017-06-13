d856f2f4b1e83e8c44fa31811a13862bb2a5a662
lizq13 <lizq13@lenovo.com>
2017-06-13 08:45:37 +0800

    [YETIN-2193][lizq13]reserve space for uid <= 10000 when storage is low
    reserve space for uid <= 10000 when storage is low
    
    Change-Id: I8a92b623cacc329e8230bdc900396afab89bd0e9
    Signed-off-by: lizq13 <lizq13@lenovo.com>

--- fs/ext4/balloc.c
+++ fs/ext4/balloc.c
@@ -532,7 +532,9 @@ static int ext4_has_free_clusters(struct ext4_sb_info *sbi,
 		return 1;
 
 	/* Hm, nope.  Are (enough) root reserved clusters available? */
-	if (uid_eq(sbi->s_resuid, current_fsuid()) ||
+	//lizq13,reserve space for uid <= s_resuid
+	if (uid_gte(sbi->s_resuid, current_fsuid()) ||
+	//lizq13,end
 	    (!gid_eq(sbi->s_resgid, GLOBAL_ROOT_GID) && in_group_p(sbi->s_resgid)) ||
 	    capable(CAP_SYS_RESOURCE) ||
 	    (flags & EXT4_MB_USE_ROOT_BLOCKS)) {
